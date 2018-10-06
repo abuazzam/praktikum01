b552010949815b38d80c0af3c54db3a0a5b60a67
Agung Nugroho <mail@chung.web.id>
2018-10-06 11:17:16 +0700

    tambah file latihan1.cpp, program hitung luas persegi panjang

---
+++ latihan1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    int p, l, luas;
+    
+    p = 10;
+    l = 5;
+    luas = p * l;
+    
+    cout << "Luasnya adalah: " << luas << endl;
+}
